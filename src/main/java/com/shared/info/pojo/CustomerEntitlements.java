9bf89ba09cc29e9fc2092f5b64035d823a876030
Suhas H C <suhashc193@gmail.com>
2024-12-18 13:08:09 +0530

    Object validator and exception handler added with BindingResult

--- src/main/java/com/shared/info/pojo/CustomerEntitlements.java
+++ src/main/java/com/shared/info/pojo/CustomerEntitlements.java
@@ -1,6 +1,7 @@
 package com.shared.info.pojo;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -9,6 +10,7 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class CustomerEntitlements {
+    @Size(min = 3, max = 15, message = "id should be between 3 and 15 only")
     private String id;
     @NotNull(message = "domicileCountry must not be null")
     private String domicileCountry;

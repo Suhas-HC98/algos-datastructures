00b30a7e47afe953fe21ad000231b3f9b8f3e73f
Suhas H C <suhashc193@gmail.com>
2024-12-21 15:54:17 +0530

    Added tests for coverage

--- src/main/java/com/shared/info/utils/Constants.java
+++ src/main/java/com/shared/info/utils/Constants.java
@@ -2,6 +2,7 @@ package com.shared.info.utils;
 
 public final class Constants {
 
+    public final static String INTERNET_PROTOCOL = "InternetProtocol";
     public final static String PERSISTABLE = "Provided request body is valid for persistence";
     public final static String NON_PERSISTABLE = "Provided request body is not valid for persistence";
 }
